bafbad77c2e98cfceefdfd384b5a8eef75924fb9
Hayden Hobbs <haydenleehobbs@gmail.com>
2018-02-05 20:14:55 -0500

    Flawed Test, but it passes

--- src/test/java/edu/bsu/cs222/jsontests.java
+++ src/test/java/edu/bsu/cs222/jsontests.java
@@ -1,4 +1,42 @@
 package edu.bsu.cs222;
 
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Map;
+
 public class jsontests {
+
+    @Test
+    public void testReadRevisions(){
+        JsonParser parser = new JsonParser();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("sample.json");
+        Reader reader = new InputStreamReader(inputStream);
+        JsonElement rootElement = parser.parse(reader);
+        JsonObject rootObject = rootElement.getAsJsonObject();
+        JsonObject pages = rootObject.getAsJsonObject("query").getAsJsonObject("pages");
+        JsonArray array = null;
+        for (Map.Entry<String,JsonElement> entry : pages.entrySet()) {
+            JsonObject entryObject = entry.getValue().getAsJsonObject();
+            array = entryObject.getAsJsonArray("revisions");
+        }
+        //JsonParser arrayParser = new JsonParser();
+        //InputStream arrayStream = getClass().getClassLoader().getResourceAsStream("user");
+        //Reader arrayReader = new InputStreamReader(arrayStream);
+        //JsonElement userName = arrayParser.parse(arrayReader);
+
+        //array.get(0).toString();
+
+        JsonElement revisionAuthor = ((JsonObject) array.get(0)).get("user");
+         //expected = "Kind Tennis Fan";
+        Assert.assertEquals(((JsonObject) array.get(0)).get("user"), revisionAuthor);
+    }
 }
