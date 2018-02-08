fabc14f9314080ffefec76459c1d4bdb2f9e980a
Hayden Hobbs <haydenleehobbs@gmail.com>
2018-02-07 19:10:02 -0500

    Production Code work; creating RevisionParser class and Revision class

--- src/main/java/edu/bsu/cs222/RevisionParser.java
+++ src/main/java/edu/bsu/cs222/RevisionParser.java
@@ -9,17 +9,19 @@ import com.google.gson.JsonParser;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class RevisionParser {
-    
+
     public List<Revision> parse (InputStream input){
         JsonParser RevisionParser = new JsonParser();
         Reader reader = new InputStreamReader(input);
         JsonElement rootElement = RevisionParser.parse(reader);
         JsonObject rootObject = rootElement.getAsJsonObject();
         JsonObject pages = rootObject.getAsJsonObject("query").getAsJsonObject("pages");
+        List<Revision> list = new ArrayList<Revision>();
         JsonArray array = null;
         for (Map.Entry<String,JsonElement> entry : pages.entrySet()) {
             JsonObject entryObject = entry.getValue().getAsJsonObject();
