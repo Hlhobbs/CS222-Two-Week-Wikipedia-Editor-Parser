d0f826d775305ae3fb90cc6785af6af830cc5c59
Matthew Taylor <mktaylor3>
2018-02-07 20:08:17 -0500

    I have added a internet connection test to WikiReader

--- src/main/java/WikiReader.java
+++ src/main/java/WikiReader.java
@@ -1,9 +1,43 @@
 import java.net.*;
 import java.io.*;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.UnknownHostException;
 //import java.util.Scanner;
 
 
 public class WikiReader {
+
+    public static void InternetConnectionTest() throws UnknownHostException, IOException
+    {
+
+        try
+        {
+
+            try
+            {
+                URL url = new URL("http://www.google.com");
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.connect();
+            }
+
+            catch (Exception exception)
+            {
+                System.out.println("No Internet Connection");
+
+            }
+
+        }
+
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+
 
     public static void main(String[] args) throws Exception
     {
@@ -12,6 +46,7 @@ public class WikiReader {
         System.out.println("What do you want to search? ");
         String search = scan.nextLine();
         */
+        InternetConnectionTest();
 
         URL wikiurl = new URL("http://en.wikipedia.org");
         URLConnection connection = wikiurl.openConnection();
